cda053cf6ef636a75be36ba35036bed2bcc7079b
Martin Noble <martinemnoble1@mac.com>
2024-05-16 11:13:26 +0100

    With proper centring of rotatio

--- src/MyARCanvas.jsx
+++ src/MyARCanvas.jsx
@@ -1,15 +1,19 @@
-import { AmbientLight, BoxGeometry, Mesh, MeshStandardMaterial, PointLight, Scene, Object3D, Vector3 } from "three"
+import { AmbientLight, BoxGeometry, Mesh, MeshStandardMaterial, PointLight, Scene, Object3D, Vector3, Quaternion } from "three"
 import { ARCanvas, ARMarker } from "@artcom/react-three-arjs"
-import { createRef, useCallback, useMemo, useRef, useState } from "react";
+import { createRef, forwardRef, useCallback, useMemo, useRef, useState } from "react";
 import { useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { InputLabel, Radio, FormGroup, FormControl, FormControlLabel } from "@mui/material";
 
-const Model = (props) => {
+const Model = forwardRef((props, ref) => {
     const targetGLTF = useLoader(GLTFLoader, `/data/${props.root}-target.glb`);
+    targetGLTF.scene.name = 'target'
     const drugGLTF = useLoader(GLTFLoader, `/data/${props.root}-drug.glb`);
+    drugGLTF.scene.name = 'drug'
     const surfaceGLTF = useLoader(GLTFLoader, `/data/${props.root}-surface.glb`);
+    surfaceGLTF.scene.name = 'surface'
     const mapGLTF = useLoader(GLTFLoader, `/data/${props.root}-map.glb`);
+    mapGLTF.scene.name = 'map'
     const gltfs = []
     const lastFrameTime = useRef(0)
 
@@ -19,7 +23,9 @@ const Model = (props) => {
     if (props.display.includes("map")) { gltfs.push(mapGLTF) }
     console.log(gltfs)
 
+    const shell = new Object3D()
     const parent = new Object3D()
+    shell.add(parent)
     const boundingBoxMiddle = new Vector3(
         (drugGLTF.scene.children[0].geometry.boundingBox.min.x + drugGLTF.scene.children[0].geometry.boundingBox.max.x) / 2,
         (drugGLTF.scene.children[0].geometry.boundingBox.min.y + drugGLTF.scene.children[0].geometry.boundingBox.max.y) / 2,
@@ -33,14 +39,16 @@ const Model = (props) => {
     })
 
     useFrame((frame) => {
-        parent.rotateY((frame.clock.elapsedTime - lastFrameTime.current) )
+        shell.rotateY((frame.clock.elapsedTime - lastFrameTime.current))
+        //rotation.w += (frame.clock.elapsedTime - lastFrameTime.current)
         lastFrameTime.current = frame.clock.elapsedTime
     })
     const scale = 0.3
-    const position = boundingBoxMiddle.multiplyScalar(-scale)
-    console.log({position, boundingBoxMiddle})
-    return <primitive object={parent} scale={scale} position={position} />
-};
+    console.log({ boundingBoxMiddle })
+    parent.translateOnAxis(boundingBoxMiddle, -1.0)
+
+    return <primitive ref={ref} object={shell} scale={scale} />
+});
 
 
 const Box = () => {
@@ -66,7 +74,7 @@ export const MyARCanvas = (props) => {
 
     const handleToggle = useCallback(async (ev) => {
         //setDisplay([])
-        console.log(theModel)
+        console.log('theModel', theModel, modelRef)
 
         //console.log(theModel.getAttribute('scale'))
         if (display.includes(ev.target.name)) {
@@ -92,6 +100,7 @@ export const MyARCanvas = (props) => {
         )}
         <ARCanvas
             key={JSON.stringify(display)}
+            //arEnabled={false}
             gl={{ antialias: true, powerPreference: "default", physicallyCorrectLights: false }}
             onCameraStreamReady={() => console.log("Camera stream ready")}
             onCameraStreamError={() => console.error("Camera stream error")}
@@ -102,7 +111,7 @@ export const MyARCanvas = (props) => {
             <pointLight position={[10, 10, 0]} intensity={1.0} />
             <ARMarker
                 params={{ smooth: true }}
-                type={"barcode"}
+                type={"unknown"} //['pattern', 'barcode', 'unknown' ]
                 barcodeValue={6}
                 //patternUrl={"data/patt.hiro"}
                 onMarkerFound={() => {
